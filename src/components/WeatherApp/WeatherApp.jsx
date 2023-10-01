6df2cf7403c8fe515711238b5773bae7aa9f0091
MATERIALBASE <applenagireddy@gmail.com>
2023-10-01 13:53:31 +0530

    Fixed Errors

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -4,7 +4,7 @@ import './WeatherApp.css';
 import search_icon from '../Assets/search.png';
 import clear_icon from '../Assets/clear.png';
 import cloud_icon from '../Assets/cloud.png';
-import drizzle_icon from '../Assets/drizzle.png';
+// import drizzle_icon from '../Assets/drizzle.png';
 import rain_icon from '../Assets/rain.png';
 import snow_icon from '../Assets/snow.png';
 import wind_icon from '../Assets/wind.png';
